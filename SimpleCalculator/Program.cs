dbe8d62450ab19353f30ca5f7aab704805303900
ZeynabNamazova <159948539+ZeynabNamazova@users.noreply.github.com>
2024-04-13 10:37:05 +0400

    Add files via upload

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+            {
+                Console.Write("Enter first number:");
+                double a = Convert.ToDouble(Console.ReadLine());
+                char symbol = Convert.ToChar(Console.ReadLine());
+                Console.Write("Enter second number:");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+
+                switch (symbol)
+                {
+                    case '+':
+                        Console.WriteLine(Convert.ToDouble(a + b) + " ",  "solve");
+                        break;
+
+                    case '-':
+                        Console.WriteLine(Convert.ToDouble(a - b) + " ",  "solve");
+                        break;
+
+                    case '*':
+                        Console.WriteLine(Convert.ToDouble(a * b) + " ",  "solve");
+                        break;
+
+                    case '/':
+                        Console.WriteLine(Convert.ToDouble(a / b) + " ",  "solve");
+                        break;
+
+                    default:
+                        Console.WriteLine("Don't the right symbol");
+                        break;
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
