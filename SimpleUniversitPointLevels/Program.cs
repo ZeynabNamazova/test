967981f70465871f9ba5803fd5e410358917b56c
ZeynabNamazova <159948539+ZeynabNamazova@users.noreply.github.com>
2024-04-13 11:23:46 +0400

    Add files via upload

---
+++ SimpleUniversitPointLevels/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleUniversitPointLevels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
