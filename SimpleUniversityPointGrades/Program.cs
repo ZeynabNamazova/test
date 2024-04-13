5ea5404343f982c75efd431ec6c0c44b0311d5e7
ZeynabNamazova <159948539+ZeynabNamazova@users.noreply.github.com>
2024-04-13 11:26:55 +0400

    Add files via upload

---
+++ SimpleUniversityPointGrades/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SimpleUniversityPointGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the point: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                if (a <= 50 && a >= 0)
+                {
+                    Console.WriteLine("F");
+                }
+                else if (a <= 60 && a > 51)
+                {
+                    Console.WriteLine("E");
+                }
+                else if (a <= 70 && a >= 61)
+                {
+                    Console.WriteLine("D");
+                }
+                else if (a <= 80 && a >= 71)
+                {
+                    Console.WriteLine("C");
+                }
+                else if (a <= 90 && a >= 81)
+                {
+                    Console.WriteLine("B");
+
+                }
+                else if (a <= 100 && a >= 91)
+                {
+                    Console.WriteLine("A");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the correct point");
+                }
+            }
+        }
+    }
+}
